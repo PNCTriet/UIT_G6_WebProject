489a8d98a2c7b3126c48aba8e38d61fa3c3f5556
Nguyen Hoang Vi <nguyenhoangvi6744@gmail.com>
2024-05-19 19:16:47 +0700

    Add ProfileController to get user information from user, save them to database for displaying in profile page

--- UIT_G6_WEBPROJECT_LARAVEL/app/Models/User.php
+++ UIT_G6_WEBPROJECT_LARAVEL/app/Models/User.php
@@ -10,7 +10,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-    protected $table = 'User';
+    protected $table = 'user';
     /**
      * The attributes that are mass assignable.
      *
